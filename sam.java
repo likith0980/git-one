9fd68ca204b35ca24b00a4d9bdd6e34cecfd2db9
likith <1likithroxx@gmail.com>
2021-11-18 21:26:32 +0530

    praneeth added

--- sam.java
+++ sam.java
@@ -3,6 +3,7 @@ public class Main
   static void main(String args[])
   {
     System.out.println("hello  hello ");
-    System.out.println("how are you")
+    System.out.println("how are you");
+    System.out.println("praneeth");
   }
 }
