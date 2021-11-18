d2977de6941be150f85c88edeb94b64d8bde2548
likith <1likithroxx@gmail.com>
2021-11-18 21:25:37 +0530

    hello hello added

--- sam.java
+++ sam.java
@@ -2,7 +2,7 @@ public class Main
 {
   static void main(String args[])
   {
-    System.out.println("hello hello ");
+    System.out.println("hello  hello ");
     System.out.println("how are you")
   }
 }
